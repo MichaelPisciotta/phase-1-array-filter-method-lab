285903cd185c3733a8a739bf213ee52f51fd32c1
MichaelPisciotta <mfp56@miami.edu>
2021-07-23 16:59:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, string){
+return drivers.filter(driver => driver.toUpperCase() === string.toUpperCase());
+}
+
+
+
+
+
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.indexOf(string) === string.indexOf(string));
+}
+
+
+
+
+
+
+
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string);
+}
